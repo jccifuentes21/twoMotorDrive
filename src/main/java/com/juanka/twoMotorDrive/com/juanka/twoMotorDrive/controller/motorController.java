93c379d921b9e5f719a5bfe91ccebb79aaa270ab
jccifuentes21 <jccifuentes21@gmail.com>
2015-10-20 22:18:28 -0500

    first commit

---
+++ src/main/java/com/juanka/twoMotorDrive/com/juanka/twoMotorDrive/controller/motorController.java
@@ -0,0 +1,32 @@
+package com.juanka.twoMotorDrive.com.juanka.twoMotorDrive.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class motorController {
+
+    @RequestMapping("/")
+    public String start () {
+
+        return "app is running ok.";
+    }
+
+    @RequestMapping("/drive")
+    public String drive () throws InterruptedException{
+
+        GpioController gpio = GpioFactory.getInstance();
+
+        GpioPinDigitalOutput leftMotor = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "Motor Izquierda", PinState.LOW);
+        GpioPinDigitalOutput rightMotor = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "Motor Derecha", PinState.LOW);
+
+
+        leftMotor.high();
+        rightMotor.high();
+        Thread.sleep(3000);
+
+        return "Program was executed correctly";
+
+    }
+}
